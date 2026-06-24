6320939ee78f89a218ee6e712db04e7010e759b4
Kathryn Wheeler <kiwheel@bu.edu>
2026-06-24 18:46:39 -0400

    fit a pruned classification tree

--- decision_trees.py
+++ decision_trees.py
@@ -1,6 +1,7 @@
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 import pandas as pd
+import numpy as np
 
 def split_data(feature_df):
     response_name = "label"
@@ -12,7 +13,28 @@ def split_data(feature_df):
     
     return x_train, x_test, y_train, y_test
 
-def fit_tree(x_train,y_train):
-    clf = DecisionTreeClassifier()
-    mdl = clf.fit(x_train, y_train)
+def fit_tree(x_train, y_train, prune=False):
+    clf = DecisionTreeClassifier(random_state=1234)
+
+    #Without pruning
+    if not prune:
+        mdl = clf.fit(x_train, y_train)
+
+    #With pruning
+    else:
+        n = len(x_train)
+        path = clf.cost_complexity_pruning_path(x_train, y_train)
+        ccp_alphas = path.ccp_alphas
+
+        grid = {
+            'max_depth':        list(range(1, int(np.log2(n)) + 1)), #1 up to log2 of sample size
+            'min_samples_split': [max(2, int(n * p)) for p in [0.005, 0.01, 0.02, 0.05]], #2-5% of sample size
+            'min_samples_leaf':  [max(1, int(n * p)) for p in [0.005, 0.01, 0.02]], #1-2% of sample size
+            'ccp_alpha':         list(np.linspace(ccp_alphas[0], ccp_alphas[-1], 10)), #Only select 10 to test
+        }
+
+        gcv = GridSearchCV(estimator=clf, param_grid=grid)
+        gcv.fit(x_train, y_train)
+        mdl = gcv.best_estimator_
+
     return mdl
